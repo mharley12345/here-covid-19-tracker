c82482b71b3878b17b1410d6f0b8e6191ca64ed6
Mike Harley <mharley12345@gmail.com>
2020-03-26 18:00:08 -0700

    .

--- covid-map/gatsby-config.js
+++ covid-map/gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#00AFAA`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cover.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
